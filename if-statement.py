7a9cbd6ea22656f8526cebf9b2e73baf651dee26
Olugbenga Ajewole <ajewolegben@gmail.com>
2024-07-18 18:44:20 +0100

    if, elif, else statement

---
+++ if-statement.py
@@ -0,0 +1,10 @@
+check = "Olugbenga"
+
+if check == "False":
+    print("This statement is False")
+elif not check:
+    print("This statement is also False")
+elif check == "Olugbenga":
+    print("Wait, Why will it be my name Olugbenga")
+else:
+    print("This statement is actually true")
